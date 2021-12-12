ee5c6b2a9fa5437415c47548f2a7f50e813a541b
Jester Lopez <Jesmark1999@gmail.com>
2021-12-12 16:57:58 -0600

    Add Deploy.sh

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/job-listings-with-filtering/",
   plugins: [react()],
   build: {
     outDir: './build'
